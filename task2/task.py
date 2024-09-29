c4869f8170c490fac06e5cab6969ac430a7aaac0
unknown <solovevis171@gmail.com>
2024-09-29 14:56:39 +0300

    upd

--- task2/task.py
+++ task2/task.py
@@ -25,6 +25,17 @@ def find_all_childs_cnt(v, edges):
         total_childs += find_all_childs_cnt(to, edges)
 
     return total_childs
+
+def find_depth(v, edges, depth_dict, d=0):
+    depth_dict.setdefault(v, d)
+
+    # если вершина - лист, то никуда дальше не идем
+    if v not in edges.keys():
+        return 0
+    
+    # ищем глубины дальше
+    for to in edges[v]:
+        find_depth(to, edges, depth_dict, d+1)
     
 
 if __name__ == "__main__":
@@ -37,9 +48,37 @@ if __name__ == "__main__":
     df = pd.read_csv(file_path, header=None)
     edges = dict()
     
+    vertices = []
     # Структура дерева
     for edge_num, edge in df.iterrows():
         from_vertice = int(edge[0])
         to_vertice = int(edge[1])
+        vertices.extend([from_vertice, to_vertice])
         edges.setdefault(from_vertice, [])
         edges[from_vertice].append(to_vertice)
+    vertices = list(set(vertices))
+    vertices = sorted(vertices)
+
+    print(vertices)
+
+    # depth_dict = dict()
+    # find_depth(1, edges, depth_dict)
+    # print(depth_dict)
+
+    final_answer = pd.DataFrame(columns=['r1', 'r2', 'r3'])
+
+    for v in vertices:
+        ans = dict.fromkeys(['r1', 'r2', 'r3'])
+        # r1 - непосредственное управление
+        ans['r1'] = find_nearest_childs_cnt(v, edges)
+        # r2 - отношение непосредственного подчинения
+        root = find_root(edges)
+        ans['r2'] = int(root != v)
+        # отношение посредственного управления
+        ans['r3'] = find_all_childs_cnt(v, edges) - ans['r1']
+
+        ans = pd.DataFrame([ans], index=[v])
+
+        final_answer = pd.concat([final_answer, ans], ignore_index=False)
+
+    print(final_answer)
